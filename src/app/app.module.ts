fa4ba16c60a49b6a13db68638e6b5e238af1deaf
tacascer <trandangtrithanh2000@gmail.com>
2020-12-03 19:04:05 -0500

    Applied a custom attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent
+    MediaItemListComponent,
+    FavoriteDirective,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/favorite.directive.ts
@@ -0,0 +1,7 @@
+import { Directive, HostBinding } from "@angular/core";
+@Directive({
+  selector: "[mwFavorite]",
+})
+export class FavoriteDirective {
+  @HostBinding("class.is-favorite") isFavorite = true;
+}
